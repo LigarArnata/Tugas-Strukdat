f96678a9ec8764200fb4c5c17415bfd0b588891c
Garrr <95403456+LigarArnata@users.noreply.github.com>
2022-06-03 06:01:20 +0700

    Add files via upload

--- 5025211244_Ligar Arsa Arnata.c
+++ 5025211244_Ligar Arsa Arnata.c
@@ -1,28 +1,29 @@
-#include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <conio.h>
 
 void main(){
 
-     int data[9] = {3,9,11,12,15,17,23,31,35};
-     int low,high;
-     int flag=0,d=35;
-     float pos1;
-     int pos, n=9;
-     low =0;
-     high =n-1;
-
-     do{
-        pos1=(float)(d-data[low])/(data[high]-data[low])*(high-low)+low;
-        pos=floor(pos1);
-        if(data[pos]==d){
-            flag=1;
-            break;
+    int data[9] = {3,9,11,12,15,17,23,31,35};
+    int l,r,m;
+    int n=9;
+    int cari=17;
+    l=0;
+    r=n-1;
+    int ktm=0;
+    while(l<=r && ktm==0){
+        m=(l+r)/2;
+        printf("data tengah : %d\n",m);
+        if(data[m]==cari)ktm=1;
+        else if (cari<data[m]){
+            printf("cari dikiri\n");
+            r=m-1;
         }
-        if(data[pos]>d) high=pos-1;
-        else
-            if (data[pos]<d) low=pos+1;
-     }while(d>=data[low]&&d<=data[high]);
-     if(flag==0) printf("Data tidak ada\n"); else printf("Data ada\n");
+        else{
+            l=m+1;
+            printf("cari dikanan\n");
+        }
+    }
+    if(ktm==1) printf("data ada\n");
+    else printf("data tidak ada\n");
+
 }
